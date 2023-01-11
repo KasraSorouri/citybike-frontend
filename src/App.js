73ef7b86d3ba216db3eee49c23475b950f657146
Kasra Sorouri <k.sorouri@gmail.com>
2023-01-11 21:14:15 +0200

    Add Navigationbar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom'
 import Stations from './components/Stations'
 import Trips from './components/Trips'
+import Navigation from './components/Navigation'
 
 function App() {
 
   return (
     <Container>
       <Router>
+        <Navigation />
         <h1>City bike Application</h1>
         <Routes>
           <Route path="/trips" element={<Trips  />} />
